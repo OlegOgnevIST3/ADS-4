20ccf663714fe26af5838de411e97b014c39277a
OlegOgnevIST3 <124896310+OlegOgnevIST3@users.noreply.github.com>
2023-03-15 19:35:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,15 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
+int countPairs1(int* arr, int len, int value) {
     int x = 0;
     for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) x++;
+            if (arr[i] + arr[j] == value)
+                x++;
         }
     }
     return x;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int* arr, int len, int value) {
     int x = 0;
     int a = len - 1;
     while (arr[a] > value) {
@@ -16,20 +17,22 @@ int countPairs2(int *arr, int len, int value) {
     }
     for (int i = 0; i < a; i++) {
         for (int j = a; j > i; j--) {
-            if (arr[i] + arr[j] == value) x++;
-            if (arr[i] + arr[j] < value) continue;
+            if (arr[i] + arr[j] == value)
+                x++;
+            if (arr[i] + arr[j] < value)
+                continue;
         }
     }
     return x;
 }
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int* arr, int len, int value) {
     int x = 0;
     for (int i = 0; i < len - 1 && arr[i] <= value; i++) {
-        x += fun_for_cp3(arr + i + 1, len - i - 1, value - arr[i]);
+        x += fun(arr + i + 1, len - i - 1, value - arr[i]);
     }
     return x;
 }
-int fun_for_cp3(int* arr, int size, int value) {
+int fun(int* arr, int size, int value) {
     int b = 0, a = size, c;
     int x = 0;
     while (b <= a) {
